a46202521d2e764f7eff8e60bd24da5035b6dba5
cslunsford <christopher.lunsford@uconn.edu>
2023-08-18 14:35:16 -0400

    installed uuid package and created basic format for server.js file

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-const api = require('./index.js');
+const uuid = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
