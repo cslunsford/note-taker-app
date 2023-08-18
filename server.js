8c6c60d1478b761429d24c8b09e506819233e1ad
cslunsford <christopher.lunsford@uconn.edu>
2023-08-18 17:03:26 -0400

    changed order of routes

--- server.js
+++ server.js
@@ -11,14 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (error, data) => {
         if (error) {
@@ -48,6 +40,14 @@ app.post('/api/notes', (req, res) => {
             });
         }
     });
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () => {
