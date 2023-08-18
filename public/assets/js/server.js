3be3f4f398d81d79953af332316661f04fd13d52
cslunsford <christopher.lunsford@uconn.edu>
2023-08-18 15:15:45 -0400

    added html and api routes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -11,6 +11,45 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db.json', 'utf8', (error, data) => {
+        if (error) {
+            res.status(500).json({ error: 'Unable to read notes.' });
+        } else {
+            const notes = JSON.parse(data);
+            res.json(notes);
+        }
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    fs.readFile('db.json', 'utf8', (error, data) => {
+        if (error) {
+            res.status(500).json({ error: 'Unable to read notes.' });
+        } else {
+            const notes = JSON.parse(data);
+            newNote.id = uuid.v4();
+            notes.push(newNote);
+            fs.writeFile('db.json', JSON.stringify(notes), (error) => {
+                if (error) {
+                    res.status(500).json({ error: 'Unable to write notes.' });
+                } else {
+                    res.json(newNote);
+                }
+            });
+        }
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
 });
